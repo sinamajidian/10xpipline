aa0f0651244ffc84eef6aef4f1f3b2a6ab17a700
Sina Majidian <sina@sinas-air.wurnet.nl>
2019-06-12 14:01:16 +0200

    backup

---
+++ draft/nfinder.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+"""
+Script to find region of non-N sequence of a FASTA file
+
+Sina Majidian
+
+Attention this version only works with on record per fasta file
+
+"""
+
+
+
+import sys
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+
+def nonNregion_extractor(seq):
+    start_pos=0
+    counter=0
+    gap=False
+    gap_length = 0  
+    start_Nregion=1
+    list_tuple_pos=[]
+    for idx, char in enumerate(seq):
+        if idx==0:       
+            if char == 'N': 
+                    start_pos=1 
+        else:
+            previous_char=seq[idx-1]
+            if char == 'N': 
+                if previous_char != 'N': # new N region started 
+                    start_pos=idx+1 #  0-index to genomic pos
+                    #else:  char =previous_char = 'N' 
+            else: # char = 'A'
+                if previous_char == 'N':
+                    lastN_idx=idx-1
+                    end_pos=lastN_idx+1
+                    list_tuple_pos.append((start_pos,end_pos))
+    list_tuple_pos_nonN=[]
+    for idx_nregion, tuple_pos in enumerate(list_tuple_pos):
+        previous_tuple_pos=list_tuple_pos[idx_nregion-1]
+        #print(previous_tuple_pos[1],tuple_pos[0])
+        list_tuple_pos_nonN.append((previous_tuple_pos[1],tuple_pos[0]))
+    list_tuple_pos_nonN.append((tuple_pos[1],len(seq)))
+    list_tuple_pos_nonN[0]=(1,list_tuple_pos[0][0]) #the 0 index is not correct, now ok
+
+    return list_tuple_pos_nonN
+
+if __name__ == "__main__":
+
+    file_fa= sys.argv[1]  # 'tst.fa'
+    with open(file_fa, mode="r") as fasta_handle:
+        for record in SeqIO.parse(fasta_handle, "fasta"):
+            seq = str(record.seq)
+    
+    list_tuple_pos_nonN=nonNregion_extractor(seq)
+    
+    file_bed = open("nonNregion.bed","a") 
+    for tuple_pos_nonN in list_tuple_pos_nonN:
+        file_bed.write(str(tuple_pos_nonN[0])+'\t'+str(tuple_pos_nonN[1])+'\n')
+    file_bed.close()
+
+    
